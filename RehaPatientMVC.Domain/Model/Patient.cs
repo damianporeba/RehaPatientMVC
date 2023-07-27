7c49dcef600147b1f4693d380801d8bf5a5c6f7d
Damian Poręba <damianporeba98@gmail.com>
2023-07-27 23:50:45 +0200

    added database termplate

---
+++ RehaPatientMVC.Domain/Model/Patient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RehaPatientMVC.Domain.Model
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Pesel { get; set; }
+        public int TypeId { get; set; }
+       
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+
+
+
+        public virtual Patient patient { get; set; }
+        public virtual ICollection<Address> Addresses { get; set; }
+    }
+}
