4a8ea81a022b3d6f4bab09207f7870bc0222d9c2
Damian Poręba <damianporeba98@gmail.com>
2023-08-01 07:00:59 +0200

    updated repositories

---
+++ RehaPatientMVC.Domain/Interface/IAddressRepository.cs
@@ -0,0 +1,17 @@
+﻿using RehaPatientMVC.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RehaPatientMVC.Domain.Interface
+{
+    public interface IAddressRepository
+    {
+        void DeleteAddress(int addressId);
+        int AddAddress(Address address);
+        int UpdateAddress(Address address);
+
+    }
+}
--- RehaPatientMVC.Infrastructure/Repositories/AddressReporitory.cs
+++ RehaPatientMVC.Infrastructure/Repositories/AddressReporitory.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RehaPatientMVC.Domain.Interface;
+using RehaPatientMVC.Domain.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +8,42 @@ using System.Threading.Tasks;
 
 namespace RehaPatientMVC.Infrastructure.Repositories
 {
-    public class AddressReporitory
+    public class AddressReporitory : IAddressRepository
     {
         private readonly Context _context;
         public AddressReporitory(Context context)
         {
             context = _context;
+        }
+
+        public int AddAddress(Address address)
+        {
+            _context.addresses.Add(address);
+            _context.SaveChanges();
+            return address.Id;
+        }
+
+        public void DeleteAddress(int addressId)
+        {
+            var address = _context.addresses.Find(addressId);
+            if (address != null)
+            {
+                _context.addresses.Remove(address);
+                _context.SaveChanges();
+            }
+
+        }
+
+        public int UpdateAddress(Address address)
+        {
+            var addressUpdate = _context.addresses.FirstOrDefault(i=>i.Id == address.Id);
+            if (address != null)
+            {
+                addressUpdate = address;
+            }
+            return addressUpdate.Id;
+
+            
         }
     }
 }
