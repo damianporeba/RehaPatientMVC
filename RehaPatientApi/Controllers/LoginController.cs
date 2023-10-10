29a47df01ab6a87b106123616fdc470a5c3aef34
Damian Poręba <damianporeba98@gmail.com>
2023-10-10 23:04:05 +0200

    RehaPatientMVC: fix authorize JWT method

--- RehaPatientApi/Controllers/LoginController.cs
+++ RehaPatientApi/Controllers/LoginController.cs
@@ -1,11 +1,62 @@
+<<<<<<< HEAD
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+=======
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+>>>>>>> d3c402acc2632db32dd5249e6d0e142355ee7cbb
 
 namespace RehaPatientApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+<<<<<<< HEAD
+    public class LoginController : ControllerBase
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IConfiguration _config;
+        public LoginController(IConfiguration config, SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+            _config = config;
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Login([FromBody] UserModel loginModel)
+        {
+            IActionResult response = Unauthorized();
+            var success = AuthenticateUser(loginModel);
+
+            if (success)
+            {
+                var tokenString = GenerateJsonWebToken(loginModel);
+                response = Ok(new {token = tokenString});
+            }
+            return response;
+        }
+
+        private string GenerateJsonWebToken(UserModel loginModel)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Issuer"], null, expires: DateTime.Now.AddMinutes(120), signingCredentials: credentials);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private bool AuthenticateUser(UserModel loginModel)
+        {
+            var result = _signInManager.PasswordSignInAsync(loginModel.Email, loginModel.Password, true, lockoutOnFailure: false).Result;
+            return result.Succeeded;
+        }
+=======
     public class ValuesController : ControllerBase
     {
+>>>>>>> d3c402acc2632db32dd5249e6d0e142355ee7cbb
     }
 }
--- RehaPatientApi/Program.cs
+++ RehaPatientApi/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+<<<<<<< HEAD
+using Microsoft.EntityFrameworkCore;
+=======
+>>>>>>> d3c402acc2632db32dd5249e6d0e142355ee7cbb
 using Microsoft.IdentityModel.Tokens;
 using RehaPatientMVC.Infrastructure;
 using System.Text;
@@ -13,22 +17,31 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+<<<<<<< HEAD
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<Context>(options =>
+    options.UseSqlServer(connectionString));
+=======
 var app = builder.Build();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<Context>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+>>>>>>> d3c402acc2632db32dd5249e6d0e142355ee7cbb
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<Context>();
 
+<<<<<<< HEAD
+=======
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+>>>>>>> d3c402acc2632db32dd5249e6d0e142355ee7cbb
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
@@ -41,6 +54,33 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidIssuer = builder.Configuration["Jwt:Issuer"],
             ValidAudience = builder.Configuration["Jwt:Issuer"],
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+<<<<<<< HEAD
+        };
+    }
+    );
+
+var app = builder.Build();
+
+
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+=======
 
         };
     }
@@ -51,6 +91,7 @@ app.UseAuthorization();
 
 app.UseAuthentication();
 
+>>>>>>> d3c402acc2632db32dd5249e6d0e142355ee7cbb
 app.MapControllers();
 
 app.Run();
