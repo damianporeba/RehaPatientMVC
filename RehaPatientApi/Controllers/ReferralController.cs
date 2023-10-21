a09cf130844d867d52828d6beb864e503a929940
Damian Poręba <damianporeba98@gmail.com>
2023-10-21 15:07:04 +0200

    RehaPatientApi: add method for view all list of referrals

--- RehaPatientApi/Controllers/ReferralController.cs
+++ RehaPatientApi/Controllers/ReferralController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RehaPatientMVC.Application.Interfaces;
+using RehaPatientMVC.Application.ViewModels;
+using RehaPatientMVC.Application.ViewModels.Referral;
 
 namespace RehaPatientApi.Controllers
 {
@@ -7,5 +10,45 @@ namespace RehaPatientApi.Controllers
     [ApiController]
     public class ReferralController : ControllerBase
     {
+        private readonly IReferralService _referralService;
+
+        public ReferralController(IReferralService referralService)
+        {
+            _referralService = referralService;
+        }
+
+        [HttpGet]
+        public ActionResult<ListReferralForListVm> Index() 
+        {
+            var model = _referralService.GetAllReferralsForList(3, 1, "");
+            if (model == null)
+            {
+                return NotFound();
+            }
+            return Ok(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(SearchInListVm searchInList)
+        {
+            var searchString = searchInList.searchString;
+            var pageNumber = searchInList.pageNumber;
+
+            if (searchString == null)
+            {
+                searchString = string.Empty;
+            }
+            if (pageNumber == 0)
+            {
+                pageNumber = 1;
+            }
+
+            var model = _referralService.GetAllReferralsForList(searchInList.pageSize, pageNumber, searchString);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            return Ok(model);
+        }
     }
 }
