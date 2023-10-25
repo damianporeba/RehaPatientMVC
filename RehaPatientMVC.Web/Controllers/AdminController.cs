92eac0833c0ac2057f0ba6be9417b378a4b35e12
Damian Poręba <damianporeba98@gmail.com>
2023-10-25 22:52:15 +0200

    RehaPatientMVC: add AdminController, IAdminService and AdminService class

---
+++ RehaPatientMVC.Web/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RehaPatientMVC.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdminController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            return View(_roleManager.Roles);
+        }
+    }
+}
