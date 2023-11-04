1307f4447df037b6081791e65cea8abd319f9a6d
Damian Poręba <damianporeba98@gmail.com>
2023-11-04 23:21:42 +0100

    RehaPatientMVC: add RoleController

---
+++ RehaPatientMVC.Web/Controllers/RoleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RehaPatientMVC.Web.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
