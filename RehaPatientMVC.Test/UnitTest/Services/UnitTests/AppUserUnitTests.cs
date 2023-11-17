d214acfbd2004d6d90be841e046e92ab7dca19a8
Damian Poręba <damianporeba98@gmail.com>
2023-11-17 23:36:41 +0100

    RehaPatientMVC: add unit test for GetAppUserForEdit in AppUserService

--- RehaPatientMVC.Test/UnitTest/Services/UnitTests/AppUserUnitTests.cs
+++ RehaPatientMVC.Test/UnitTest/Services/UnitTests/AppUserUnitTests.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
+using FluentAssertions;
 using Microsoft.CodeAnalysis.Differencing;
 using Moq;
 using RehaPatientMVC.Application.Services;
+using RehaPatientMVC.Application.ViewModels.Medics;
 using RehaPatientMVC.Application.ViewModels.UserApp;
 using RehaPatientMVC.Domain.Interface;
 using RehaPatientMVC.Domain.Model;
@@ -48,6 +50,44 @@ namespace RehaPatientMVC.Test.UnitTest.Services.UnitTests
 
             //Assert
             mockRepository.Verify(x=>x.AddNewAppUser(appUser));
+        }
+
+        [Fact]
+        public void CheckAppUserForEditAreEqualLikeModel()
+        {
+            //Arrange
+            AppUser appUser = new AppUser()
+            {
+                Id = 1,
+                City = "Tarnów",
+                UserLastName = "Test",
+                UserFirstName = "Test"
+            };
+
+            NewAppUserVm newAppUserVm = new NewAppUserVm()
+            {
+                Id = 1,
+                City = "Tarnów",
+                UserLastName = "Test",
+                UserFirstName = "Test"
+            };
+
+            var mockMapper = new Mock<IMapper>();
+            mockMapper.Setup(x => x.Map<NewAppUserVm>(It.IsAny<AppUser>())).Returns(newAppUserVm);
+
+            var mockRepository = new Mock<IAppUserRepository>();
+            mockRepository.Setup(x => x.GetAppUserById(1)).Returns(appUser);
+
+            var manager = new AppUserService(mockRepository.Object, mockMapper.Object);
+
+            //Act
+            var returnedMedic = manager.GetAppUserForEdit(10);
+
+            //Assert
+            returnedMedic.UserFirstName.Should().Be(appUser.UserFirstName);
+            returnedMedic.UserLastName.Should().Be(appUser.UserLastName);
+            returnedMedic.City.Should().Be(appUser.City);
+            returnedMedic.Id.Should().Be(appUser.Id);
         }
     }
 }
