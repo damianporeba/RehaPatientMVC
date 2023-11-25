887d1d01df6e37763ff5b858f560d0abf4de6485
Damian Poręba <damianporeba98@gmail.com>
2023-11-25 15:21:45 +0100

    RehaPatientMVC: add unit test for AddPatient method in PatientRepository

--- RehaPatientMVC.Test/UnitTest/Repositories/PatientRepositoryUnitTest.cs
+++ RehaPatientMVC.Test/UnitTest/Repositories/PatientRepositoryUnitTest.cs
@@ -1,12 +1,81 @@
-﻿using System;
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using RehaPatientMVC.Domain.Model;
+using RehaPatientMVC.Infrastructure;
+using RehaPatientMVC.Infrastructure.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace RehaPatientMVC.Test.UnitTest.Repositories
 {
-    internal class PatientRepositoryUnitTest
+    public class PatientRepositoryUnitTest
     {
+        [Fact]
+        public void CheckReturnedIdAfterAddNewPatient()
+        {
+            //Arrange
+            var patient = new Patient
+            {
+                Id = 10,
+                Name = "Test",
+                LastName = "Test",
+                Pesel = "98989898989"
+            };
+
+            var options = new DbContextOptionsBuilder<Context>()
+                .UseInMemoryDatabase(databaseName: "RehaPatientMVC")
+                .Options;
+
+            //Act
+            using (var context = new Context(options))
+            {
+                var patientRepository = new PatientRepository(context);
+                var patientResult = patientRepository.AddPatient(patient);
+
+                //Assert
+                patientResult.Should().NotBe(null);
+            }
+        }
+
+        [Fact]
+        public void CheckIsPatientExistAfterRemove() 
+        {
+            var patient = new Patient
+            {
+                Id = 10,
+                Name = "Test",
+                LastName = "Test",
+                Pesel = "98989898989"
+            };
+
+            var patientToRemove = new Patient
+            {
+                Id = 11,
+                Name = "Test",
+                LastName = "Test",
+                Pesel = "98989898989"
+            };
+
+            var options = new DbContextOptionsBuilder<Context>()
+                .UseInMemoryDatabase(databaseName: "RehaPatientMVC")
+                .Options;
+
+            //Act
+            using (var context = new Context(options))
+            {
+                var patientRepository = new PatientRepository(context);
+                patientRepository.DeletePatient(patientToRemove.Id);
+                var patientDetails = patientRepository.GetPatientById(11);
+
+                //Assert
+
+                patientDetails.Should().BeNull();
+            }
+        }
+
     }
 }
