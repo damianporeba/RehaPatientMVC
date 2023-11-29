a921bcaf3d4cea1c9570d256b4fd437027d6e731
Damian Poręba <damianporeba98@gmail.com>
2023-11-29 20:33:13 +0100

    RehaPatientMVC: add unit test for AddNewAppUser method in AppUserRepository

--- RehaPatientMVC.Test/UnitTest/Repositories/AppUserRepositoryUnitTest.cs
+++ RehaPatientMVC.Test/UnitTest/Repositories/AppUserRepositoryUnitTest.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using RehaPatientMVC.Domain.Model;
+using RehaPatientMVC.Infrastructure.Repositories;
+using RehaPatientMVC.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
 
 namespace RehaPatientMVC.Test.UnitTest.Repositories
 {
-    internal class AppUserRepositoryUnitTest
+    public class AppUserRepositoryUnitTest
     {
+        [Fact]
+        public void CheckAppUserExistAfterAdd()
+        {
+            //Arrange
+            var appUser = new AppUser
+            {
+                Id = 10,
+                UserFirstName = "Test",
+                UserLastName = "Test",
+                City = "Tarnów"
+            };
+
+            var options = new DbContextOptionsBuilder<Context>()
+                .UseInMemoryDatabase(databaseName: "RehaPatientMVC")
+                .Options;
+
+            //Act
+            using (var context = new Context(options))
+            {
+                var appUserRepository = new AppUserRepository(context);
+                appUserRepository.AddNewAppUser(appUser);
+                var result = appUserRepository.GetAppUserById(10);
+
+                //Assert
+                result.Should().Be(appUser);
+            }
+        }
     }
 }
