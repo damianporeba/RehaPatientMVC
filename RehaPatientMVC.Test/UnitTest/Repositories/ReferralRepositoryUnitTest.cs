d3a46fdd14ef8bce6322cad83b6a5a3d05dde689
Damian Poręba <damianporeba98@gmail.com>
2023-11-29 20:17:55 +0100

    RehaPatientMVC: add unit test for AddReferral method in ReferralRepository

--- RehaPatientMVC.Test/UnitTest/Repositories/ReferralRepositoryUnitTest.cs
+++ RehaPatientMVC.Test/UnitTest/Repositories/ReferralRepositoryUnitTest.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis.Differencing;
+using Microsoft.EntityFrameworkCore;
+using RehaPatientMVC.Domain.Model;
+using RehaPatientMVC.Infrastructure.Repositories;
+using RehaPatientMVC.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
 
 namespace RehaPatientMVC.Test.UnitTest.Repositories
 {
-    internal class ReferralRepositoryUnitTest
+    public class ReferralRepositoryUnitTest
     {
+        [Fact]
+        public void CheckReferralExistAfterAdd()
+        {
+            Referral referral = new Referral
+            {
+                Id = 10,
+                ICD10 = "M54",
+                Code = "0000",
+                Pesel = "00000000000",
+                PatientId = 1,
+                MedicId = 1,
+                TypeReferral = "Ambulatory"
+            };
+            var options = new DbContextOptionsBuilder<Context>()
+                .UseInMemoryDatabase(databaseName: "RehaPatientMVC")
+                .Options;
+
+            //Act
+            using (var context = new Context(options))
+            {
+                var refRepository = new ReferralRepository(context);
+                var medicResult = refRepository.AddReferral(referral);
+
+                //Assert
+                medicResult.Should().Be(10);
+            }
+        }
+
     }
 }
