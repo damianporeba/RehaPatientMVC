fc91625e794032bcacbe8c26b293db41e18aad61
Damian Poręba <damianporeba98@gmail.com>
2023-12-02 13:28:15 +0100

    RehaPatientApi: fix routing for methods in Patient Controller

--- RehaPatientApi/Controllers/PatientController.cs
+++ RehaPatientApi/Controllers/PatientController.cs
@@ -17,7 +17,7 @@ namespace RehaPatientApi.Controllers
             _patientService = patientService;
         }
 
-        [HttpGet("Index")]
+        [HttpGet]
         [AllowAnonymous]
         public ActionResult<ListPatientForListVm> Index()
         {
@@ -29,30 +29,30 @@ namespace RehaPatientApi.Controllers
             return Ok(model);
         }
 
-        [HttpPost]
-        [AllowAnonymous]
-        public ActionResult<ListPatientForListVm> Index([FromBody] SearchInListVm searchVm)
-        {
-            var pageNumber = searchVm.pageNumber;
-            var searchString = searchVm.searchString; ;
+        //[HttpPost("Index")]
+        //[AllowAnonymous]
+        //public ActionResult<ListPatientForListVm> Index([FromBody] SearchInListVm searchVm)
+        //{
+        //    var pageNumber = searchVm.pageNumber;
+        //    var searchString = searchVm.searchString; ;
 
-            if (pageNumber == 0)
-            {
-                pageNumber = 1;
-            }
+        //    if (pageNumber == 0)
+        //    {
+        //        pageNumber = 1;
+        //    }
 
-            if (searchString == null)
-            {
-                searchString = string.Empty;
-            }
+        //    if (searchString == null)
+        //    {
+        //        searchString = string.Empty;
+        //    }
 
-            var model = _patientService.GetAllPatientsForList(searchVm.pageSize, pageNumber, searchString);
-            if (model == null)
-            {
-                return NotFound();
-            }
-            return Ok(model);
-        }
+        //    var model = _patientService.GetAllPatientsForList(searchVm.pageSize, pageNumber, searchString);
+        //    if (model == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return Ok(model);
+        //}
 
         [HttpPost]
         public ActionResult AddPatient([FromBody] NewPatientVm newPatientVm)
@@ -61,30 +61,30 @@ namespace RehaPatientApi.Controllers
             return RedirectToAction("Index");
         }
 
-        [HttpGet("/{id}")]
+        [HttpGet("{id}")]
         public ActionResult PatientDetails (int id)
         {
             var details = _patientService.ViewPatientDetails(id);
             return Ok(details);
         }
 
-        [HttpDelete("/{id}")]
+        [HttpDelete("{id}")]
         public ActionResult DeletePatient(int id)
         {
             _patientService.DeletePatient(id);
             return RedirectToAction("Index");
         }
 
-        [HttpGet("/{id}")]
-        public ActionResult<NewPatientVm> EditPatient(int id)
-        {
-            var model = _patientService.GetPatientForEdit(id);
-            if (model  == null) 
-            {
-            return NotFound();
-            }
-            return Ok(model);
-        }
+        //[HttpPatch("/{id}")]
+        //public ActionResult<NewPatientVm> EditPatient(int id)
+        //{
+        //    var model = _patientService.GetPatientForEdit(id);
+        //    if (model == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return Ok(model);
+        //}
 
         [HttpPut]
         public ActionResult EditPatient([FromBody] NewPatientVm newPatientVm)
